ebf721d450a36bbf6ac1b5816454fc007e20ea41
jortizme <jortizmez@gmail.com>
2020-04-09 13:29:24 +0200

    "config.c erkennt jetzt sowohl den Befehl als auch die Parameters"

--- config.c
+++ config.c
@@ -33,46 +33,56 @@ void read_command()
     char *buffer = (char *)malloc(sizeof(char)*INPUT_SIZE); //check if the spac is right allocate
     char path_command[16];
     char cmd[16]; 
-    char parameter[16];
-    int counter = 0;
-    char *token;
- 
+    char pmt[16];
+    char *token = NULL;
+    char *pre_token = NULL;//stores the last token
+
     strcpy(path_command,"/bin/");
 
-    fgets(buffer, INPUT_SIZE, stdin);
+    fgets(buffer, INPUT_SIZE, stdin);//die Zeile wird eingegeben
 
     token = strtok(buffer, " ");
     strcpy(cmd,token);
-    strcat(path_command,cmd);
-
-    while(token != NULL)
+    token = strtok(NULL, " ");//Nochmals um zu pruefen ob ein parameter da ist
+     
+    while( token != NULL)
     {
-        ++counter;
-        if(counter == 2)
-            strcpy(parameter,token);
-        token = strtok( NULL," ");
+        pre_token = token; //pre_token stores the last word of the line before enter
+        token = strtok(NULL, " ");
     }
 
-    if(token == NULL && counter == 1)
-        strcpy(parameter, " ");
+    if(pre_token == NULL)//falls immer noch pre_token sich noch nicht geändert hat  
+    {
+        token = strtok(cmd,"\n");
+        strcpy(cmd,token);
+        strcpy(pmt, " ");
+    }  
+    else
+    {
+        token = strtok(pre_token,"\n");  
+        strcpy(pmt,token);
+    }    
 
-    exec_cmd(path_command,cmd,parameter);
+    strcat(path_command,cmd);
+    exec_cmd(path_command,cmd,pmt);
 
-    free(buffer);
-    
+    free(buffer); 
 }
 
-void exec_cmd(const char *path_command, char *command,  char *parameter) 
+void exec_cmd(const char *path_command, char *cmd,  char *pmt) 
 {
     int status;
     pid_t pid,x;
 
-    char *cmd[] = {command, parameter, (char*)NULL};
+    char *command[] = {cmd, pmt, (char*)NULL};
 
-   printf("This is the path:%s\n",path_command);
-    printf("THis is the command:%s\n",command);
-    printf("THis is the paramater:%s\n", parameter);
-
+    /*printf("This is the path:%s\n",path_command);
+    printf("Lenght of path: %ld\n", strlen(path_command));
+    printf("THis is the command:%s\n",cmd);
+    printf("Lenght of command: %ld\n", strlen(cmd));
+    printf("THis is the paramater:%s\n", pmt);
+    printf("Lenght of parameter: %ld\n", strlen(pmt));
+*/
 
     pid = fork();
 
@@ -91,8 +101,8 @@ void exec_cmd(const char *path_command, char *command,  char *parameter)
     }
     else
     {
-        if((execv(path_command,cmd) < 0))
+        if((execv(path_command,command) < 0))
             perror("execv failed");
     }
-   
+  
 }
